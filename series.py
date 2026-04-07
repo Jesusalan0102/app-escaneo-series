5b18c91f71be8caa3f7cf00f859c0e72dcbb634a
Jesusalan0102 <jesus.alan.sepulveda.soto82@uabc.edu.mx>
2026-04-07 14:13:07 -0700

    Add files via upload

--- series.py
+++ series.py
@@ -15,7 +15,7 @@ CARRIER_LIGHT_BLUE = "#00A9E0" # Azul claro
 CARRIER_GRAY = "#F4F4F4"
 
 # Logo
-LOGO_URL = LOGO_URL = "https://raw.githubusercontent.com/Jesusalan0102/app-escaneo-series/main/carrierlogo.jpeg"  
+LOGO_URL = LOGO_URL = https://github.com/Jesusalan0102/app-escaneo-series/blob/main/carrierlogo.jpeg.jpg  
 # ← Cambia TU-USUARIO por tu usuario real de GitHub
 
 # CSS Profesional Carrier
